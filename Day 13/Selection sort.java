c169dd9fe3607b138b2ae6b82b0cae72865f9a02
Karthik <karthik3904@gmail.com>
2024-10-07 19:33:18 +0530

    Create Selection sort.java

---
+++ Day 13/Selection sort.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    int  select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+        int mini=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[mini]) mini=j;
+        }
+        return mini;
+    }
+    void selectionSort(int arr[],int n){
+        //code here
+        for(int i=0;i<n-1;i++){
+            int mini=select(arr,i);
+            int Temp = arr[i];
+            arr[i] = arr[mini];
+            arr[mini]=Temp;
+        }
+    }
+}
