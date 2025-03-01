5edddcf0b9d34f00994e0561ce563f168397954d
Karthik P <karthik3904@gmail.com>
2025-03-01 18:45:51 +0530

    Create Sum of Natural Numbers.java

---
+++ Day 40/Sum of Natural Numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int seriesSum(int n) {
+        int res=0;
+        for(int i=1;i<=n;i++){
+          res=res+i;  
+        }
+        return res;
+        
+    }
+}
