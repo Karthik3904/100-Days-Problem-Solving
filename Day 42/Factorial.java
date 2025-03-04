db620de43dcf9712880e3cf13a4394a3642ff5a1
Karthik P <karthik3904@gmail.com>
2025-03-04 21:23:28 +0530

    Create Factorial.java

---
+++ Day 42/Factorial.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int factorial(int n) {
+        if(n==0 || n==1) return 1;
+        int res=1;
+        for(int i=2;i<=n;i++){
+            res=res*i;
+        }
+        return res;
+    }
+}
