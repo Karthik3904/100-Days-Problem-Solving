1dcd9df5a5c3b28391e17573f178821662fddf6b
Karthik P <karthik3904@gmail.com>
2025-03-06 22:06:25 +0530

    Create Array Duplicates.java

---
+++ Day 44/Array Duplicates.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]) list.add(arr[i]);
+            
+            
+        }
+        return list;
+    }
+}
