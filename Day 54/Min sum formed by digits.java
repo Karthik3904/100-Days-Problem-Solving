4766a0c093228c10d1c46b44483fa5f854f58b25
Karthik P <karthik3904@gmail.com>
2025-03-18 22:07:05 +0530

    Update Min sum formed by digits.java

--- Day 54/Min sum formed by digits.java
+++ Day 54/Min sum formed by digits.java
@@ -1,3 +1,29 @@
+/*Given an array of digits (values are from 0 to 9), find the minimum possible sum of two numbers formed from digits of the array. All digits of the given array must be used to form the two numbers.
+
+Any combination of digits may be used to form the two numbers to be summed.  Leading zeroes are permitted.
+
+If forming two numbers is impossible (i.e. when n==0) then the "sum" is the value of the only number that can be formed.
+
+
+ 
+
+ 
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {6, 8, 4, 5, 2, 3}
+Output:
+604
+Explanation:
+The minimum sum is formed by numbers 
+358 and 246*/
+
+
+
+
+
 class Solution {
     
     public static long minSum(int arr[], int n)
