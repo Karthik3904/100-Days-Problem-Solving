bb6cac744b6f9829686f52bfdb5dc0bdbee4e48d
Karthik P <karthik3904@gmail.com>
2025-03-19 21:37:18 +0530

    Create Reverse an Array.java

---
+++ Day 55/Reverse an Array.java
@@ -0,0 +1,18 @@
+/*Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.*/
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        int l=0 , r=arr.length-1;
+        while(l<r){
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+            
+        }
+    }
+}
