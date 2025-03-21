c3fa5e07f234fc9f7c024b08fd2a90251fa46098
Karthik P <karthik3904@gmail.com>
2025-03-21 20:14:21 +0530

    Update Intersection of Arrays with Distinct.java

--- Day 56/Intersection of Arrays with Distinct.java
+++ Day 56/Intersection of Arrays with Distinct.java
@@ -1,3 +1,13 @@
+/*Examples:
+
+Input: a[] = [89, 24, 75, 11, 23], b[] = [89, 2, 4]
+Output: 1
+Explanation: 89 is the only element in the intersection of two arrays.
+Input: a[] = [1, 2, 4, 3, 5, 6], b[] = [3, 4, 5, 6, 7]
+Output: 4
+Explanation: 3, 4, 5, and 6 are the elements in the intersection of two arrays.*/
+
+
 class Solution {
     public static int numberofElementsInIntersection(int a[], int b[]) {
         HashSet<Integer>set1=new HashSet<>();
